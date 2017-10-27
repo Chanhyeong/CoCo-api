ddb552dcebfdca7f7348325df7b145da3375f814
Chanhyeong Cho <chu7825@gmail.com>
2017-10-27 18:54:03 +0900

    change query string of chat

--- routes/api/chat/controller.js
+++ routes/api/chat/controller.js
@@ -1,28 +1,29 @@
 var mysql = require('../../../middleware/database')('mysql');
 var mongodb = require('../../../middleware/database')('mongodb');
-
-/**
- * req.body: id, message;
- */
+var model = require('../board/model')
 
 exports.getList = function (req, res) {
     var nickname = req.user.nickname;
 
-    var listQuery = "select * from Class where tutorNick = ? or studentNick = ?";
+    var statement = "select num, title, tutorNick, studentNick " +
+        "from Class where tutorNick = ? OR studentNick = ? AND status - ?";
 
-    mysql.query(listQuery, nickname, nickname, function(err, result){
+    mysql.query(statement, nickname, nickname, model.getConst('ON'), function(err, result){
         if (err) {
             res.status(404).json(err)
         } else {
             if(result.length !== 0) {
-                res.status(204).json(null);
-            }
-            else {
-                res.status(200).json(result)
+                res.status(204).json({ list: null });
+            } else {
+                res.status(200).json({ list: result });
             }
         }
     });
 };
+
+/**
+ * req.body: id, message;
+ */
 
 exports.getMessageLog = function (req, res) {
     var classNumber = parseInt(req.params.classNumber);
@@ -30,7 +31,7 @@ exports.getMessageLog = function (req, res) {
     mongodb.getMessage(req.params.mode, classNumber, function (result) {
         process.nextTick( function () {
             if(result) {
-                res.status(200).send(result.log);
+                res.status(200).send({ log : result.log });
             } else {
                 res.status(401).send('Error; Get msg');
             }
@@ -40,6 +41,8 @@ exports.getMessageLog = function (req, res) {
 
 exports.sendMessage = function (req, res) {
     var classNumber = parseInt(req.params.classNumber);
+
+    // Format: 2017-10-27 17:19:33
     var current = new Date().toISOString().
     replace(/T/, ' ').      // replace T with a space
     replace(/\..+/, '');     // delete the dot and everything after
