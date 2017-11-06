a334731f8ec6cefb00efb87201d6618c6df3c2a8
hhk <hhk@hhkui-MacBook-Air.local>
2017-11-06 12:26:29 +0900

    튜터 등록 추가

--- routes/api/user/controller.js
+++ routes/api/user/controller.js
@@ -7,7 +7,7 @@ exports.getUser = function (req, res) {
     var sql = "select id, email, nickname, tutor from User where id = ?";
     mysql.query(sql, userID, function (err, result) {
         if (err) {
-            res.status(404).json({error: err})
+            res.status(500).json({error: err})
         } else {
             res.status(200).json(result[0]);
         }
@@ -21,7 +21,7 @@ exports.getClass = function (req, res) {
     var sql = "select * from Class where studentNick = ? or tutorNick = ? and status = 3";
     mysql.query(sql, [userID, userID], function (err, result) {
         if (err) {
-            res.status(404).json({error: err})
+            res.status(500).json({error: err})
         } else {
             if (!result.length) {
                 res.status(401).json("수강중인 목록이 없습니다");
@@ -40,7 +40,7 @@ exports.getWriter = function(req, res) {
     var sql = "select * from Chat as ch, Class as c where ch.classNum = c.num and writer = ?";
     mysql.query(sql, userID, function (err, result) {
         if (err) {
-            res.status(404).json({error: err})
+            res.status(500).json({error: err})
         } else {
             if (!result.length) {
                 res.status(401).json("신청한 받은 목록이 없습니다.");
@@ -58,7 +58,7 @@ exports.getApplicant = function(req, res){
     var sql = "select * from Chat as ch, Class as c where ch.classNum = c.num and applicant = ?";
     mysql.query(sql, userID, function(err, result){
         if (err) {
-            res.status(404).json({ error: err })
+            res.status(500).json({ error: err })
         } else {
             if(!result.length) {
                 res.status(401).json("신청한 목록이 없습니다.");
@@ -68,3 +68,24 @@ exports.getApplicant = function(req, res){
         }
     });
 };
+
+exports.regist = function(req, res){
+    var userID = req.params.id;
+
+    var info = {
+        'degree' : req.body.degree,
+        'intro' : req.body.intro,
+        'github' : req.body.github,
+        'career' : req.body.career
+    };
+
+    var sql = "insert into Tutor values (?, ?, ?, ?, ?)";
+
+    mysql.query(sql, [userID, info.degree, info.intro, info.github, info.career] , function(err, result){
+        if (err) {
+            res.status(500).json({ error: err })
+        } else {
+            res.status(200).json(result);
+        }
+    });
+}
--- routes/api/user/router.js
+++ routes/api/user/router.js
@@ -2,13 +2,10 @@ var express = require('express');
 var router = express.Router();
 var controller = require('./controller')
 
-
 router.get('/getUser/:id', controller.getUser);
 router.get('/getClass/:id', controller.getClass);
 router.get('/getWriter/:id', controller.getWriter);
 router.get('/getApplicant/:id', controller.getApplicant);
-router.post('/');
-router.put('/');
-router.delete('/');
+router.post('/tutor/:id', controller.regist);
 
 module.exports = router;
