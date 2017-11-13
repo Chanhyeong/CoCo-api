87f3d133d712ce35bc74ccf94aa6669d65e09186
Chanhyeong Cho <chu7825@gmail.com>
2017-11-13 20:33:11 +0900

    test for access control allow headers

--- middleware/index.js
+++ middleware/index.js
@@ -20,7 +20,9 @@ Middleware.init = function (server, app) {
 
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers: Origin, Accept, X-Requested-With, Content-Type," +
+            "Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
         next();
     });
 
