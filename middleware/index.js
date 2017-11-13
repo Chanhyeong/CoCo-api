7a8ccd915672ced562d6e470bf53baa412d792fc
Chanhyeong Cho <chu7825@gmail.com>
2017-11-13 20:25:06 +0900

    add Access-Control-Allow-Origin option

--- middleware/index.js
+++ middleware/index.js
@@ -16,7 +16,13 @@ Middleware.init = function (server, app) {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cors())
+    app.use(cors());
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
     // TODO 1: 프로젝트 생성 시 소켓 생성하도록
     // TODO 2: 시작 시 모든 포트에 대한 소켓을 열기 or 프로젝트 접속자 파악해서 열고 닫기
