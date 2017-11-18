4fe39259371563fe83d44926ddc5683ba62fa461
hhk <hhk@hhkui-MacBook-Air.local>
2017-11-18 17:07:53 +0900

    user 쿼리 수정

--- routes/api/user/controller.js
+++ routes/api/user/controller.js
@@ -18,7 +18,7 @@ exports.getUser = function (req, res) {
 };
 
 exports.getClass = function (req, res) {
-    var userID = req.params.id;
+    var userNick = req.params.nickname;
 
     var status = {
         'STUDENT': 1,
@@ -29,7 +29,7 @@ exports.getClass = function (req, res) {
 
     // 내가 수강중인 목록
     var sql = "select * from Class where studentNick = ? or tutorNick = ? and status = ?";
-    var filter = [userID, userID, status.MATCHED];
+    var filter = [userNick, userNick, status.MATCHED];
 
     mysql.query(sql, filter, function (err, result) {
         if (err) {
@@ -48,7 +48,7 @@ exports.getClass = function (req, res) {
 };
 
 exports.getWriter = function(req, res) {
-    var filter = req.params.id;
+    var filter = req.params.nickname;
 
     // 신청온 목록
     var sql = "select * from Chat as ch, Class as c where ch.classNum = c.num and writer = ?";
@@ -68,7 +68,7 @@ exports.getWriter = function(req, res) {
 };
 
 exports.getApplicant = function(req, res){
-    var filter = req.params.id;
+    var filter = req.params.nickname;
 
     // 신청한 목록
     var sql = "select * from Chat as ch, Class as c where ch.classNum = c.num and applicant = ?";
--- routes/api/user/router.js
+++ routes/api/user/router.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var controller = require('./controller')
 
 router.get('/getUser/:id', controller.getUser);
-router.get('/getClass/:id', controller.getClass);
-router.get('/getWriter/:id', controller.getWriter);
-router.get('/getApplicant/:id', controller.getApplicant);
+router.get('/getClass/:nickname', controller.getClass);
+router.get('/getWriter/:nickname', controller.getWriter);
+router.get('/getApplicant/:nickname', controller.getApplicant);
 router.get('/TutorInfo/:id', controller.TutorInfo);
 router.get('/getTutor/:nickname', controller.getTutor);
 router.post('/tutor', controller.regist);
