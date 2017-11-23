34d969a1a95983683dca38002827f63a6fd2a7c4
hhk <hhk2945@naver.com>
2017-11-23 17:41:09 +0900

    디렉토리 update 초기 구현

--- routes/api/pad/directory/controller.js
+++ routes/api/pad/directory/controller.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 
 
 exports.getDir = function (req, res) {
-    exec('tree -J ~/store/' + req.params.classNum , function (err, stdout, stderr){
+    exec('tree -J ~/store/' + req.params.classNum , function (err, stdout){
         if (err) {
             console.log('exec error : tree error');
             res.status(500).send('Err: exec tree error');
@@ -20,20 +20,37 @@ exports.update = function (req, res) {
     var classNum = req.body.classNum;
     var mode = req.body.mode;
     var path = req.body.path;
+    var newpath = req.body.newpath;
+    var fileName = req.body.fileName;
+
+    var statement = 'docker exec ' + classNum + ' bash -c "cd ' + path +' && ';
 
     switch (mode) {
         case 'create':
-            exec('docker exec ' + classNum + '');
+            if(req.body.Dir){
+                statement =+ 'mkdir ' + fileName +'"';
+            } else {
+                statement =+ 'touch ' + fileName +'"';
+            }
             break;
+
         case 'rename':
-
+            statement =+ 'mv ' + fileName +'"';
             break;
+
         case 'delete':
-
+            if(req.body.Dir){
+                statement =+ 'rm -r' + fileName +'"';
+            } else {
+                statement =+ 'rm ' + fileName +'"';
+            }
             break;
-        case 'move':
 
+        case 'move':
+            statement =+ 'mv ' + fileName + ' ' + newpath+'"';
             break;
         default: console.log('wrong mode'); return false;
     }
+
+    exec(statement);
 };
