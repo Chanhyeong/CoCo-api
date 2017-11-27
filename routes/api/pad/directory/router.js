c89e83675fc6c207f1e17c889adbb2dac1b6ab34
Chanhyeong Cho <chu7825@gmail.com>
2017-11-27 19:43:22 +0900

    delete unused method

--- routes/api/pad/directory/router.js
+++ routes/api/pad/directory/router.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var controller = require('./controller');
 var jwtHandler = require('../../../../middleware/jwt-handler');
 
-router.get('/:classNum', jwtHandler.decodeToken , controller.getDir);
 router.put('/', jwtHandler.decodeToken, controller.update);
 
 module.exports = router;
