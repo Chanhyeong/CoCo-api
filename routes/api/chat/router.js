3d776fa734b200f8939e7fa41eab07f8a2fb6f79
Chanhyeong Cho <chu7825@gmail.com>
2017-12-21 17:30:50 +0900

    add missed jwt decode

--- routes/api/chat/router.js
+++ routes/api/chat/router.js
@@ -5,8 +5,8 @@ var jwtHandler = require('../../../middleware/jwt-handler');
 
 router.get('/list', jwtHandler.decodeToken, controller.getMessages);
 router.get('/:chatNumber', jwtHandler.decodeToken, controller.getMessage);
-router.put('/request/:chatNumber', controller.handleMatch);
+router.put('/request/:chatNumber', jwtHandler.decodeToken, controller.handleMatch);
 router.put('/:chatNumber', jwtHandler.decodeToken, controller.sendMessage);
-router.delete('/:chatNumber', controller.delete);
+router.delete('/:chatNumber', jwtHandler.decodeToken, controller.delete);
 
 module.exports = router;
